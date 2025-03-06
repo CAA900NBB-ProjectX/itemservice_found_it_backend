3a3eafdd781e3ff3d5c7738faff96766e3724ac5
amendafernando <mail2amenda@gmail.com>
2025-03-06 13:25:44 -0500

    new DTO added

---
+++ src/main/java/com/projectx/foundit/dto/CategoryDto.java
@@ -0,0 +1,16 @@
+package com.projectx.foundit.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDto {
+    private String personal;
+    private String electronics;
+    private String documents;
+    private String clothing;
+    private String pets;
+    private String other;
+
+}
